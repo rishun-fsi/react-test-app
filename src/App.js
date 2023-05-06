23b15d6c681c9de7224fd9ca0ae19d1ea79cc64f
lisyunn <lisyunn@gmail.com>
2023-05-06 15:15:39 +0900

    my first change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, CircularProgress, AppBar, Toolbar, Typography, Badge, Avatar } from '@mui/material';
+import { Mail, Assignment } from '@mui/icons-material'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
+
+  const getSpLoginUser = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">
+
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Container maxWidth="sm">
+      <p>Create React Appで作ったよ</p>
+      <p>すでにSharePointにログイン(サインイン)しているので特別な認証設定なしにRestも使えます</p>
+      <Button variant="contained" color="primary" onClick={getSpLoginUser}>
+        SharePoint Onlineのログインユーザを画面上部に表示
+      </Button>
+    </Container>
+    <Container maxWidth="sm">
+      <p>material ui のコンポーネントも使えるよ</p>
+      <div>
+        <p>CircularProgress</p>
+        <CircularProgress />
+      </div>
+      <div>
+        <p>Badge</p>
+        <Badge badgeContent={4} color="secondary">
+          <Mail />
+        </Badge>
+      </div>
+      <div>
+        <p>Avatar</p>
+        <Avatar>
+          <Assignment/>
+        </Avatar>
+      </div>
+      <div>
+        <p>などなどたくさん！！！</p>
+      </div>
+    </Container>
+  </div>
   );
 }
 
