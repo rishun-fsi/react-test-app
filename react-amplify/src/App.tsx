7bbb8bfd45582ec1b6751f5eef62b299401291ab
rishun <rishun@fsi.co.jp>
2023-09-21 15:07:18 +0900

    add react-amplity

---
+++ react-amplify/src/App.tsx
@@ -0,0 +1,74 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import { Amplify, Auth, Hub } from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    region: 'ap-northeast-1',
+    userPoolId: 'ap-northeast-1_zAZE04G8D',
+    userPoolWebClientId: '3a748sbdtsdk64c92fhgcpr0uv',
+    oauth: {
+      domain: 'web-test-app.auth.ap-northeast-1.amazoncognito.com',
+      scope: ['openid'],
+      redirectSignIn: 'http://localhost:3000/',
+      redirectSignOut: 'http://localhost:3000/',
+      responseType: 'code'
+    }
+  }
+})
+
+const App = () => {
+  const [user, setUser] = useState<any | null>(null);
+
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then(userData => setUser(userData));
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
+
+    getUser().then(userData => setUser(userData));
+  }, []);
+
+  const getUser = async () => {
+    try {
+      const userData = await Auth.currentAuthenticatedUser();
+      // デバッグ用
+      Auth.currentSession().then((data) => {
+        console.log(`token: ${data.getIdToken().getJwtToken()}`);
+      });
+      console.log(userData);
+      return userData;
+    } catch (e) {
+      return console.log('Not signed in');
+    }
+  }
+
+  return user ? (
+    <div>
+      <p>サインイン済み</p>
+      <p>ユーザー名: {user.username}</p>
+      <button onClick={() => Auth.signOut()}>Sign Out</button>
+    </div>
+  ) : (
+    <div>
+      <p>
+        サインインする
+      </p>
+      <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+    </div>
+  );
+}
+
+export default App
+
